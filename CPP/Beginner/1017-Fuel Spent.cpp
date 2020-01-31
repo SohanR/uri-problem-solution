4b99931a7a604753e625951fff3c4ea72023c49a
Mizanur Rahman <mizan.rahman66d@gmail.com>
2020-02-01 02:50:31 +0600

    1017

---
+++ CPP/Beginner/1017-Fuel Spent.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double l,v,t,f;
+
+    f=12;
+
+    cin>>t;
+    cin>>v;
+
+
+    l=(t*v)/f;
+
+
+    cout<<fixed<<setprecision(3) <<l<< endl;
+
+
+
+
+
+return 0;
+}
+
