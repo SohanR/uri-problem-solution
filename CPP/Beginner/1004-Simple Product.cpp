47c86e316edd49df771e72690a15a37cb26b8ead
SohanR <mizan.rahman66d@gmail.com>
2020-03-20 02:39:45 +0600

    1004

---
+++ CPP/Beginner/1004-Simple Product.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,PROD;
+    PROD = 0;
+
+    cin>>a>>b;
+
+    PROD = a * b;
+
+    cout<<"PROD = "<<PROD<<endl;
+
+return 0;
+}
