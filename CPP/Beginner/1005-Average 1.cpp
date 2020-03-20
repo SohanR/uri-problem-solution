c73190bf6eedf5ec51e04f76668102ce787b9bb6
SohanR <mizan.rahman66d@gmail.com>
2020-03-20 19:05:00 +0600

    1005

---
+++ CPP/Beginner/1005-Average 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double A,B,m;
+
+    cin>>A>>B;
+
+    m = ((A*3.5)+(B*7.5))/(3.5+7.5);
+
+    cout<<"MEDIA = "<<m<<endl;
+
+    return 0;
+}
