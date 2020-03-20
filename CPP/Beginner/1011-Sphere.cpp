fa1f693f7fcd88f402b20d9fd3f94e53b3b1da79
SohanR <mizan.rahman66d@gmail.com>
2020-03-20 21:12:21 +0600

    1011

---
+++ CPP/Beginner/1011-Sphere.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    double R,vol,pi=3.14159;
+
+    cin>>R;
+
+    vol = (4/3.0)*pi*R*R*R;
+
+    cout<<"VOLUME = "<<fixed<<setprecision(3)<<vol<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
